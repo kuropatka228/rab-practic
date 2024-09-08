8680642ba35886dfb8339d84efac71339a5e00e9
kuropatka228 <egorshumilo25@gmail.com>
2024-09-08 21:59:23 +0900

    amvera-daq-x-electrobus-run-zab-eco

--- admin/footer/footer.php
+++ admin/footer/footer.php
@@ -1,7 +1,7 @@
 <?php
 $user = "root";
 $password = "";
-$host = "localhost";
+$host = "amvera-daq-x-electrobus-run-zab-eco";
 $db = "zab-eco";
 $dbh = 'mysql:host=' . $host . ';dbname=' . $db . ';charset=utf8';
 $pdo = new PDO($dbh, $user, $password);
